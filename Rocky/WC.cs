7f897ff6a45ee3f53238c07aeede30805c911961
WerpNSK <mininnick@yandex.ru>
2023-03-31 12:57:20 +0700

    Добавьте файлы проекта.

---
+++ Rocky/WC.cs
@@ -0,0 +1,12 @@
+﻿namespace Rocky
+{
+    //Web constants
+    public static class WC
+    {
+        public static string ImagePath = @"\images\product\";
+        public static string SessionCart = "ShoppingCartSession";
+
+        public static string AdminRole = "Admin";
+        public static string CustomerRole = "Customer";
+    }
+}
